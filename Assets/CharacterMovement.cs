f87e1e4865bdcc47fe3335f3a0f43588a35f6e7c
gamesketches <sam.von.ehren@gmail.com>
2016-03-21 23:27:49 -0400

    First commit, basic prototype

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+
+	CharacterController controller;
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float hori = Input.GetAxis("Horizontal");
+		float vert = Input.GetAxis("Vertical");
+
+		controller.Move(transform.forward * vert * Time.deltaTime * 10.0f);
+		transform.Rotate(0f, hori * 2f, 0f);
+	}
+}
---
+++ Assets/RayCastColor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayCastColor : MonoBehaviour {
+
+	Renderer myRenderer;
+	// Use this for initialization
+	void Start () {
+		myRenderer = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		Ray PsychicRay = new Ray(transform.position, transform.forward);
+
+		if(Physics.Raycast(PsychicRay, out hit, 400f)){
+			myRenderer.material.color = hit.collider.gameObject.GetComponent<Renderer>().material.color;
+		}
+		Debug.DrawRay(transform.position, transform.forward * 100f);
+	}
+}
